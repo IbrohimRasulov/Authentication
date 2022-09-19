b0246fa69a69e352dafbd68b5db252c7c4d691f8
IbrohimRasulov <ibrohim05062000@gmail.com>
2022-09-19 19:31:01 +0500

    Install Tailwind css

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/*.html",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
